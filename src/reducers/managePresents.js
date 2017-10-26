1509f8dfd37babd3cef779327cf28c552d525197
Jennifer Volpe <jennifervolpe@Jennifers-MacBook-Pro.local>
2017-10-26 10:32:14 -0400

    Done.

--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,14 @@
+let state = {numberOfPresents: 0}
+let action = {type: 'INCREASE'}
+
+
 export function managePresents(state, action){
+switch (action.type) {
+  case 'INCREASE':
+      return {...state, numberOfPresents: state.numberOfPresents + 1
+}
+
+  default:
+  return state;
+  }
 }
